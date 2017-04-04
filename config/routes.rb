34f37557cdaa90ec10cd8d82cbe6e355b73d293f
Avinash <justavi217@gmail.com>
2017-04-04 21:46:38 +0000

    Up To Date | Cannot figure out :title

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/books', to: 'books#home'
+  get '/books/new', to: 'books#new'
+  post '/books', to: 'books#create'
+  get '/books/:id/edit', to: 'books#edit'
+  put '/books/:id', to: 'books#update'
+  get '/books/:id', to: 'books#show'
 end
